578ea57de68bd8d896dabe9195a2577a90fd2e89
Jonathan Cohen <jcohen66@optonline.net>
2020-01-13 22:02:36 -0500

    Added psrc and hwsrc

--- network_scanner.py
+++ network_scanner.py
@@ -4,9 +4,17 @@ import scapy.all as scapy
 
 
 def scan(ip):
+    # Use ARP to ask who has target IP
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-#    scapy.ls(scapy.ARP())
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print('----------------------------')
+    # scapy.ls(scapy.]Ether())
 
 
-scan('10.0.2.1')
+scan('10.0.2.0/24')
