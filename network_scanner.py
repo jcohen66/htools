e5a061df3706675d7152706ac6b5130fa10f4a6f
Jonathan Cohen <jcohen66@optonline.net>
2020-01-13 22:55:50 -0500

    Added dicts and argument parsing

--- network_scanner.py
+++ network_scanner.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env ython3
 
 import scapy.all as scapy
+import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--iprange', dest='ip', help='IP or range to scan.')
+    (options, arguments) = parser.parse_args()
+    if not options.ip:
+        parser.error('[-] Please specify an ip or range. Use --help for more info.')
+
+    return options
 
 def scan(ip):
     # Use ARP to ask who has target IP
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print('----------------------------')
-    # scapy.ls(scapy.]Ether())
+        client_dict = {"ip":element[1].psrc, 'mac':element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+    
 
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------------------")
+    for element in results_list:
+        print(element['ip'] + '\t\t' + element['mac'])
 
-scan('10.0.2.0/24')
+options = get_arguments()
+
+scan_result = scan(options.ip)
+print_result(scan_result)
