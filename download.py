c2b97baa711270062234fcf0ebae0b329eb253a3
Jonathan Cohen <jcohen66@optonline.net>
2020-01-23 20:46:45 -0500

    Added tempdir and file remove

--- download.py
+++ download.py
@@ -1,9 +1,17 @@
 import requests
+import os
+import tempfile
+
 
 def download(url):
     get_response = requests.get(url)
     file_name = url.split('/')[-1]
     with open(file_name, 'wb') as out_file:
         out_file.write(get_response.content)
+    os.remove(file_name)
 
-download('https://image.freepik.com/free-photo/attractive-woman-speaking-smartphone_23-2147848964.jpg')
+
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
+url = 'https://image.freepik.com/free-photo/attractive-woman-speaking-smartphone_23-2147848964.jpg'
+download(url)
